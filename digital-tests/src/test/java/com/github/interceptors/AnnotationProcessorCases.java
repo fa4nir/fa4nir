4260b5add74c9999d780daa5af041057c6974c6e
hellcrow <bakamakafoqq@gmail.com>
2023-02-25 09:48:35 +0200

    add case number 3

--- digital-tests/src/test/java/com/github/interceptors/AnnotationProcessorCases.java
+++ digital-tests/src/test/java/com/github/interceptors/AnnotationProcessorCases.java
@@ -72,4 +72,23 @@ class AnnotationProcessorCases {
                 .generatesSources(output);
     }
 
+    @Test
+    void givenInputAnnotationClass_whenRunProcessor_thenReceivedOutputFile_3() throws URISyntaxException, IOException {
+        String inputFileName = "input/input-stab-3.java";
+        String outputFileName = "output/output-stab-3.java";
+        JavaFileObject input = JavaFileObjects.forSourceString(
+                "com.github.interceptors.CaseCustomListenerSpec", loadJavaFileAsString(inputFileName)
+        );
+        JavaFileObject output = JavaFileObjects.forSourceString(
+                "com.github.interceptors.impl.TransmitterTemplateBeanCaseZeroImpl", loadJavaFileAsString(outputFileName)
+        );
+        Truth.assert_()
+                .about(JavaSourcesSubjectFactory.javaSources())
+                .that(List.of(input))
+                .processedWith(new TransmitterProcessor())
+                .compilesWithoutError()
+                .and()
+                .generatesSources(output);
+    }
+
 }
