ee2290906bf613e8115c4aeecb600c5f361b25f6
hellcrow <bakamakafoqq@gmail.com>
2023-02-28 22:40:21 +0200

    add another description and change FetchParam and add docs to FetchParam and Transmitter

--- core/src/main/java/io/github/fa4nir/core/annotations/Transmitter.java
+++ core/src/main/java/io/github/fa4nir/core/annotations/Transmitter.java
@@ -5,14 +5,38 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation mark interface or abstract class as template
+ * of our future listener. And contains necessary methods to generate
+ * class with implementation of listener.
+ *
+ * @author Andrii
+ * @version 1.0.0
+ */
 @Target({ElementType.TYPE, ElementType.PACKAGE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Transmitter {
 
+    /**
+     * This field should contain bean name of out implementation.
+     *
+     * @return future generated class name
+     */
     String beanName();
 
+    /**
+     * This field should contain receiver id.
+     *
+     * @return class receiver id
+     */
     String receiverName();
 
+    /**
+     * If true - base implementation of listener.
+     * If false - current implementation.
+     *
+     * @return true/false
+     */
     boolean isSupper() default true;
 
 }
